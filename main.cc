87148fce1a2d50fa25cb73231bb41deef662bdc8
Justin <jdietz224@gmail.com>
2023-03-31 16:05:04 -0500

    Updated sphere intersection function.

--- main.cc
+++ main.cc
@@ -10,14 +10,14 @@
 
 double hit_sphere(const raytrace::point3& center, double radius, const raytrace::ray& r) {
     raytrace::vec3 oc = r.origin() - center;
-    auto a = dot(r.direction(), r.direction());
-    auto b = 2.0 * dot(oc, r.direction());
-    auto c = dot(oc, oc) - radius*radius;
-    auto discriminant = b*b - 4*a*c;
+    auto a = r.direction().length_squared();
+    auto half_b = dot(oc, r.direction());
+    auto c = oc.length_squared() - radius*radius;
+    auto discriminant = half_b*half_b - a*c;
     if (discriminant < 0) {
         return -1.0;
     } else {
-        return (-b - sqrt(discriminant) ) / (2.0*a);
+        return (-half_b - sqrt(discriminant) ) / a;
     }
 }
 
