ed986b73540adceb10f07e96b69df90f75515250
Justin <jdietz224@gmail.com>
2023-04-12 12:20:54 -0500

    Added a random number function to rtweekend.h

--- include/rtweekend.h
+++ include/rtweekend.h
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <random>
 
 #include "ray.h"
 #include "vec3.h"
@@ -24,5 +25,11 @@ namespace raytrace
 
     inline double degrees_to_radians(double degrees) {
         return degrees * pi / 180.0;
+    }
+
+    inline double random_double(){
+        static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+        static std::mt19937_64 generator;
+        return distribution(generator);
     }
 }
